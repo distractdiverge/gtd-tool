8b856b2b8007464b321946daa9c96dda09473d5c
Alex Lapinski <contact@alexlapinski.name>
2020-02-15 23:54:45 -0500

    Removed temporary project exploring serverlessbackend

--- packages/gtd-mobile/App.tsx
+++ packages/gtd-mobile/App.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,3 +9,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+// TODO: Remove comment and get actual return type
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+}
